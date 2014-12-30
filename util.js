038002e430325aed4daff9cf949a8307b1c6a773
Ethan Chan <metakirby5@gmail.com>
2014-12-29 21:22:13 -0800

    fix boolOrUndefined

--- util.js
+++ util.js
@@ -1,14 +1,15 @@
 /**
  * Parses a boolean if possible, returns undefined otherwise
- * @param str The string to parse
- * @returns   The parsed boolean if possible, undefined otherwise
+ * @param    str                  The string to parse
+ * @returns  boolean | undefined  The parsed boolean if possible, undefined otherwise
  */
 module.exports.boolOrUndefined = function(str) {
+  var num = +str;
   if (str !== 'true' ||
       str !== 'false' ||
-      +str !== 0 ||
-      +str !== 1)
+      num !== 0 ||
+      num !== 1)
     return undefined;
   else
-    str = isNaN(+str) ? str === 'true' : +str === 1;
+    return isNaN(num) ? str === 'true' : num === 1;
 };
